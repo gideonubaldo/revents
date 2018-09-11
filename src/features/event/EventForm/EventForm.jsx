76b87491f14715dd1dab172853b20da3879f3214
Gideon <gideon.ubaldo@sjsu.edu>
2018-09-11 16:24:19 -0700

    google map integration and auto completion for the city and the venue

--- src/features/event/EventForm/EventForm.jsx
+++ src/features/event/EventForm/EventForm.jsx
@@ -9,6 +9,7 @@ import TextInput from "../../../app/common/form/TextInput";
 import TextArea from "../../../app/common/form/TextArea";
 import SelectInput from "../../../app/common/form/SelectInput";
 import DateInput from "../../../app/common/form/DateInput";
+import PlaceInput from "../../../app/common/form/PlaceInput";
 import moment from 'moment';
 
 
@@ -103,13 +104,15 @@ class EventForm extends Component {
             <Field
               name='city'
               type='text'
-              component={TextInput}
+              component={PlaceInput}
+              options={{types: ['(cities)']}}
               placeholder='Event City'
             />
             <Field
               name='venue'
               type='text'
-              component={TextInput}
+              options={{types: ['establishment']}}
+              component={PlaceInput}
               placeholder='Event Venue'
             />
             <Field
