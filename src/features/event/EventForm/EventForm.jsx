ffc37456c83f617257b36258bb76b9202e90764a
Gideon <gideon.ubaldo@sjsu.edu>
2018-09-10 20:28:02 -0700

    added drop down menu to form f79

--- src/features/event/EventForm/EventForm.jsx
+++ src/features/event/EventForm/EventForm.jsx
@@ -6,6 +6,7 @@ import { connect } from "react-redux";
 import { createEvent, updateEvent } from "../eventActions";
 import TextInput from "../../../app/common/form/TextInput";
 import TextArea from "../../../app/common/form/TextArea";
+import SelectInput from "../../../app/common/form/SelectInput";
 
 const mapState = (state, ownProps) => {
   const eventId = ownProps.match.params.id;
@@ -31,6 +32,15 @@ const actions = {
   createEvent,
   updateEvent
 };
+
+const category = [
+    {key: 'drinks', text: 'Drinks', value: 'drinks'},
+    {key: 'culture', text: 'Culture', value: 'culture'},
+    {key: 'film', text: 'Film', value: 'film'},
+    {key: 'food', text: 'Food', value: 'food'},
+    {key: 'music', text: 'Music', value: 'music'},
+    {key: 'travel', text: 'Travel', value: 'travel'},
+];
 
 class EventForm extends Component {
 
@@ -67,7 +77,8 @@ class EventForm extends Component {
             <Field
               name='category'
               type='text'
-              component={TextInput}
+              component={SelectInput}
+              options={category}
               placeholder='What is your event about'
             />
             <Field
