3c10c57fc7a2429142beee518a91e9010f812e39
Gideon <gideon.ubaldo@sjsu.edu>
2018-09-12 16:56:47 -0700

    added modal

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -10,11 +10,13 @@ import EventDetailedPage from '../../features/event/EventDetailed/EventDetailedP
 import UserDetailedPage from '../../features/user/UserDetailed/UserDetailedPage'
 import HomePage from '../../features/home/HomePage'
 import TestComponent from '../../features/testarea/TestComponent'
+import ModalManager from '../../features/modals/ModalManager'
 
 class App extends Component {
   render() {
     return (
       <div>
+        <ModalManager/>
         <Switch>
           <Route exact path='/' component={HomePage} />
         </Switch>
--- src/features/testarea/TestComponent.jsx
+++ src/features/testarea/TestComponent.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Button, Icon } from "semantic-ui-react";
+import { Button } from "semantic-ui-react";
 import Script from "react-load-script";
-import GoogleMapReact from "google-map-react";
 import { incrementCounter, decrementCounter } from "./testActions";
 import PlacesAutocomplete, {
   geocodeByAddress,
   getLatLng
 } from "react-places-autocomplete";
+import { openModal } from '../modals/modalActions'
+
 
 const mapState = state => ({
   data: state.test.data
@@ -15,10 +16,9 @@ const mapState = state => ({
 
 const actions = {
   incrementCounter,
-  decrementCounter
+  decrementCounter,
+  openModal
 };
-
-const Marker = () => <Icon name="marker" size="big" color="red" />;
 
 class TestComponent extends Component {
   static defaultProps = {
@@ -55,17 +55,18 @@ class TestComponent extends Component {
       onChange: this.onChange
     };
 
-    const { incrementCounter, decrementCounter, data } = this.props;
+    const { incrementCounter, decrementCounter, data, openModal } = this.props;
     return (
       <div>
-        {/* <Script
+        <Script
           url='https://maps.googleapis.com/maps/api/js?key=AIzaSyAnxWjyWxIAmlZTKynyLxcAFjed45YQAb4&libraries=places'
           onLoad={this.handleScriptLoad}
-        /> */}
+        />
         <h1>Test Area</h1>
         <h3>The answer is: {data}</h3>
         <Button onClick={incrementCounter} color="green" content="Increment" />
         <Button onClick={decrementCounter} color="red" content="Decrement" />
+        <Button onClick={() => openModal('TestModal', {data: 43})} color="teal" content="Open Modal" />
         <br />
         <br />
         <form onSubmit={this.handleFormSubmit}>
@@ -74,17 +75,6 @@ class TestComponent extends Component {
           )}
           <button type="submit">Submit</button>
         </form>
-        <div style={{ height: "300px", width: "100%" }}>
-          <GoogleMapReact
-            bootstrapURLKeys={{
-              key: 'AIzaSyAnxWjyWxIAmlZTKynyLxcAFjed45YQAb4'
-            }}
-            defaultCenter={this.props.center}
-            defaultZoom={this.props.zoom}
-          >
-            <Marker lat={59.955413} lng={30.337844} text={"Kreyser Avrora"} />
-          </GoogleMapReact>
-        </div>
       </div>
     );
   }
